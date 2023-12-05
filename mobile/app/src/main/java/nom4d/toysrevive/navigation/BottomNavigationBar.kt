d61436d361e332f445b7601a0307c1d62ddb6d3e
Arthur <arthur.adam@epitech.eu>
2023-12-05 17:07:37 +0100

    Navigation: Removing Screen Name to identify screen by route name

--- mobile/app/src/main/java/nom4d/toysrevive/navigation/BottomNavigationBar.kt
+++ mobile/app/src/main/java/nom4d/toysrevive/navigation/BottomNavigationBar.kt
@@ -1,43 +1,39 @@
 package nom4d.toysrevive.navigation
 
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
-
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
-
 import nom4d.toysrevive.R
 
 @Composable
-fun BottomNavigationBar(navController: NavController, modifier: Modifier = Modifier) {
+fun BottomNavigationBar(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
     val navigationItems = listOf(
         NavigationItem.WishListScreen,
         NavigationItem.HomeScreen,
-        NavigationItem.ToyAdCreationScreen
+        NavigationItem.ToyAdCreationScreen,
+        NavigationItem.ProfileScreen
     )
 
     val navigationItemIcons = listOf(
         Icons.Filled.Favorite,
         ImageVector.vectorResource(id = R.drawable.baseline_toys_24),
-        Icons.Filled.Add
+        Icons.Filled.Add,
+        Icons.Filled.Person
     )
 
     BottomAppBar(
@@ -49,8 +45,7 @@ fun BottomNavigationBar(navController: NavController, modifier: Modifier = Modif
 
         navigationItems.forEachIndexed { index, item ->
             NavigationBarItem(
-                icon = { Icon(navigationItemIcons[index], contentDescription = item.screenName) },
-                label = { Text(text = item.screenName) },
+                icon = { Icon(navigationItemIcons[index], contentDescription = "") },
                 alwaysShowLabel = false,
                 selected = currentRoute == item.routeName,
                 onClick = {
@@ -65,9 +60,6 @@ fun BottomNavigationBar(navController: NavController, modifier: Modifier = Modif
                     }
                 }
             )
-            if (index < navigationItems.size - 1) {
-                Divider(modifier = Modifier.padding(2.dp).fillMaxHeight().width(1.dp))
-            }
         }
     }
 }
--- mobile/app/src/main/java/nom4d/toysrevive/navigation/NavigationItem.kt
+++ mobile/app/src/main/java/nom4d/toysrevive/navigation/NavigationItem.kt
@@ -1,20 +1,21 @@
 package nom4d.toysrevive.navigation
 
 sealed class NavigationItem(
-    val screenName: String,
     val routeName: String,
     val appBarDisplayName: String
 ) {
-    data object WishListScreen : NavigationItem("Saved Toys", "Wish List", "Liked Toys")
-    data object HomeScreen : NavigationItem("Swipe Screen", "Home", "")
-    data object ToyAdCreationScreen : NavigationItem("Give a toy", "Ad Creation", "Give a toy")
+    data object WishListScreen : NavigationItem("Wish List", "Liked Toys")
+    data object HomeScreen : NavigationItem("Home", "")
+    data object ToyAdCreationScreen : NavigationItem("Ad Creation", "Give a toy")
+    data object ProfileScreen : NavigationItem("Profile", "Profile")
 }
 
 fun getRouteDisplayName(routeName: String?): String? {
     val availableRoutes = listOf(
         NavigationItem.HomeScreen,
         NavigationItem.WishListScreen,
-        NavigationItem.ToyAdCreationScreen
+        NavigationItem.ToyAdCreationScreen,
+        NavigationItem.ProfileScreen
     )
 
     if (routeName == null) {
