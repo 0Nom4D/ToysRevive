d733800280fd588aa5d473da07034d0cc47d2740
Arthur <arthur.adam@epitech.eu>
2024-01-31 14:51:40 +0100

    Deps: Updating Dagger-Hilt dependencies to compile

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.kapt")
-    id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
+    id("kotlin-android")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -83,11 +83,11 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
+    implementation("com.auth0.android:jwtdecode:2.0.2")
 
     // Dagger / Hilt Dependencies
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
-    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
