0e2afd31dff73a02205c7383503ba1d38131c40c
Arthur <arthur.adam@epitech.eu>
2024-01-31 15:09:40 +0100

    Lint: Fixing lint issues for class type

--- mobile/app/src/main/java/nom4d/toysrevive/ToysReviveApp.kt
+++ mobile/app/src/main/java/nom4d/toysrevive/ToysReviveApp.kt
@@ -5,7 +5,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ToysReviveApp: Application() {
+class ToysReviveApp : Application() {
     override fun onCreate() {
         super.onCreate()
     }
