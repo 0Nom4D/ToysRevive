45c833cd17455d19d71102d73240be7485bf85b3
Arthur <arthur.adam@epitech.eu>
2024-02-08 11:23:08 +0100

    API: Adding default value to RegisterDto

--- mobile/app/src/main/java/nom4d/toysrevive/api/dtos/RegisterDto.kt
+++ mobile/app/src/main/java/nom4d/toysrevive/api/dtos/RegisterDto.kt
@@ -1,10 +1,10 @@
 package nom4d.toysrevive.api.dtos
 
 data class RegisterDto(
-    var lastName: String,
-    var firstName: String,
-    var email: String,
-    var userName: String,
-    var phone: String,
-    var password: String
+    var lastName: String = "",
+    var firstName: String = "",
+    var email: String = "",
+    var userName: String = "",
+    var phone: String = "",
+    var password: String = ""
 )
